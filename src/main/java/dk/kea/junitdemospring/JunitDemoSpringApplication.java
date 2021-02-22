6dc4a5d97e85db061a394f7e0b21d4929bca66d6
jarltuxen <jarl.tuxen@gmail.com>
2021-02-22 15:45:18 +0100

    initial commit

---
+++ src/main/java/dk/kea/junitdemospring/JunitDemoSpringApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.junitdemospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitDemoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitDemoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/junitdemospring/JunitDemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.junitdemospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitDemoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
